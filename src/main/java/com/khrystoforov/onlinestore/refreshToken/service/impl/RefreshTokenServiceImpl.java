91b56bda504e927da4460643c123da83495dfa06
HrisKirill <hristoforow.kirill@gmail.com>
2025-03-24 18:45:48 +0200

    add logging for refresh token service

--- src/main/java/com/khrystoforov/onlinestore/refreshToken/service/impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/khrystoforov/onlinestore/refreshToken/service/impl/RefreshTokenServiceImpl.java
@@ -6,29 +6,34 @@ import com.khrystoforov.onlinestore.refreshToken.service.RefreshTokenService;
 import com.khrystoforov.onlinestore.user.model.User;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class RefreshTokenServiceImpl implements RefreshTokenService {
     private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
     public RefreshToken createRefreshToken(RefreshToken refreshToken) {
+        log.info("Create refresh token {}", refreshToken);
         return refreshTokenRepository.save(refreshToken);
     }
 
     @Override
     public RefreshToken findByIdAndExpiresAtAfter(UUID refreshToken, LocalDateTime now) {
+        log.info("Find by id {} and expires after {}", refreshToken, now);
         return refreshTokenRepository.findByIdAndExpiresAtAfter(refreshToken, now)
                 .orElseThrow(() -> new EntityNotFoundException(String.format("Refresh token %s does not found", refreshToken)));
     }
 
     @Override
     public void revokeUserRefreshToken(User user) {
+        log.info("Revoke user {} refresh token", user.getId());
         refreshTokenRepository.deleteByUserId(user.getId());
     }
 
